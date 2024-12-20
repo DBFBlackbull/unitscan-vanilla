8578b918f6f04178977a063b9da462198b03a64f
Peter Bjerg Lidegaard <dbf.black.bull@gmail.com>
2024-12-20 20:43:45 +0100

    - Added list of dungeon rares - Added git ignore

---
+++ rares.lua
@@ -0,0 +1,59 @@
+-- List of rare mobs
+-- Currently only contain rares found in dungeons
+-- npc ID is only for easy look up on wowhead. It is not used in the code.
+
+unitscan_rares = {
+	-- Deadmines
+	["Miner Johnson"] = 3586,
+
+	-- Wailing Caverns
+	["Deviate Faerie Dragon"] = 5912,
+
+	-- Shadowfang Keep
+	["Deathsworn Captain"] = 3872,
+
+	-- The Stockade
+	["Bruegal Ironknuckle"] = 1720,
+
+	-- Gnomeregan
+	["Dark Iron Ambassador"] = 6228,
+
+	-- Scarlet Monastery - Graveyard
+	["Azshir the Sleepless"] = 6490,
+	["Fallen Champion"] = 6488,
+	["Ironspine"] = 6489,
+
+	-- Razorfen Kraul
+	["Earthcaller Halmgar"] = 4842,
+	["Blind Hunter"] = 4425,
+
+	-- Razorfen Downs
+	["Ragglesnout"] = 7354,
+
+	-- Maraudon
+	["Meshlok the Harvester"] = 12237,
+
+	-- Zul'Farrak
+	["Sandarr Dunereaver"] = 10080,
+	["Dustwraith"] = 10081,
+	["Zerillis"] = 10082,
+
+	-- Blackrock Depths
+	["Panzor the Invincible"] = 8923,
+
+	-- Stratholme
+	["Skul"] = 10393,
+	["Stonespine"] = 10809,
+
+	-- Lower Blackrock Spire
+	["Bannok Grimaxe"] = 9596, -- Arcanite Reaper
+	["Burning Felguard"] = 10263,
+	["Crystal Fang"] = 10376,
+	["Ghok Bashguud"] = 9718,
+	["Spirestone Lord Magus"] = 9217,
+	["Spirestone Battle Lord"] = 9218,
+	["Spirestone Butcher"] = 9219,
+
+	-- Upper Blackrock Spire
+	["Jed Runewatcher"] = 10509,
+}
